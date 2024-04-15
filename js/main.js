bc6c10b9715eb0ff3d4e806af930645bac91b2a6
Tricky71 <nowwhat@mail.ru>
2024-04-15 12:37:29 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    }
+  })
+})
+
+
+
